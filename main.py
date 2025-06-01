c26b0d75c311f66d988875058093c28f363e40a2
FaiqErlang <155573422+Faiq1818@users.noreply.github.com>
2025-06-01 19:43:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from colorama import Back, Fore, Style, init
 from game.api import Api
 from game.board_handler import BoardHandler
 from game.bot_handler import BotHandler
-from game.logic.random import RandomLogic
 from game.util import *
 from game.logic.base import BaseLogic
 from game.logic.crawler import Crawler
@@ -14,7 +13,6 @@ init()
 BASE_URL = "http://localhost:3000/api"
 DEFAULT_BOARD_ID = 1
 CONTROLLERS = {
-    "Random": RandomLogic,
     "Crawler": Crawler,
 }
 
